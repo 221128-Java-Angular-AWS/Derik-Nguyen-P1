47a73a7bf27410c65499964acf36f3e1d4ba9ef5
Derik N <derik802@revature.net>
2022-12-09 17:44:55 -0500

    added properties to gitignore

---
+++ P1/src/main/java/com/revature/Functions.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public abstract class Functions {
+    String full_name;
+    String password;
+    public abstract boolean login(String login, String password );
+
+
+
+
+
+
+
+
+
+}
---
+++ P1/src/main/java/com/revature/Manager.java
@@ -0,0 +1,18 @@
+package com.revature;
+
+import com.revature.Functions;
+
+public class Manager extends Functions {
+    String fullName;
+    String password;
+
+
+
+
+    public boolean viewPendingRequest(double amount) {
+        return true;
+    }
+    public boolean login(String login, String password ) {
+        return true;
+    }
+}
