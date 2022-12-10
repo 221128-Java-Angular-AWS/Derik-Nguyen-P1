01d83909e146f1faf766fd0f497162b1908661fe
Derik N <derik802@revature.net>
2022-12-10 14:20:48 -0500

    added connector

--- P1/src/main/java/com/revature/Main.java
+++ P1/src/main/java/com/revature/Main.java
@@ -2,13 +2,14 @@ package com.revature;
 
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello");
-        ObjectMapper obj = new ObjectMapper();
+        ConnectionManager.test();
+
 
 
 
